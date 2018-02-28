0f3fcf9bc4da625cf6e9cf0710f810ca20cc037d
Syknapse <syk.houdeib@gmail.com>
2018-02-28 18:27:22 +0100

    Major refactoring
    Create array to store fetched data.
    Refactor search results to a separate function.
    Remove 'description' from `title` as it is causing strange behaviour with text and links.
    Rewrite expand() function calling data directly from array

--- script.js
+++ script.js
@@ -1,41 +1,48 @@
 const input = document.getElementById('user-search');
 const output = document.getElementById('output-display');
 const nasaApi = 'https://images-api.nasa.gov/';
+let imageHref;
+let imageDescription;
+const imagesResults = [];
 
 input.addEventListener('keypress', function(event) {
     if(event.key === 'Enter'){
         fetch(`${nasaApi}search?q=${input.value}&media_type=image`)
         .then(response => response.json())
-        .then(data => {
-            let searchResults = '';
-            let imagesArray = data.collection.items;
-            console.log(data);
-
-            imagesArray.forEach(function(result){
-                let imageHref = result.links[0].href;
-                let imageDescription = result.data[0].description;
-                searchResults += `<img src=${imageHref} title="${imageDescription}">`;
-            })
-            output.innerHTML = searchResults;
-            output.addEventListener('click', expand);
-        })
+        .then(data => imagesResults.splice(0, ...data.collection.items))
+        .then(showImages)
         .catch(error => console.log(error))
     }
 });
 
-function expand(event) {
-    let thisImageUrl = event.target.src;
-    let thisImageDescription = event.target.title;
+function showImages() {
+    let searchResults = '';
+
+    imagesResults.forEach(result => {
+        imageHref = result.links[0].href;
+        imageDescription = result.data[0].description;
+        searchResults += `<img src=${imageHref}>`;
+    })
+    output.innerHTML = searchResults;
+    output.addEventListener('click', expand);
+
+}
+
+function expand() {
+    let clickedImageUrl = event.target.src;
+    let thisImage = imagesResults.filter(image => clickedImageUrl === image.links[0].href);
+    const description = thisImage["0"].data["0"].description;
+    const imageUrl = thisImage["0"].links["0"].href;
     const expandedImage = document.querySelector('.expanded-image');
 
     expandedImage.classList.add('show');
     expandedImage.innerHTML =
     `<span>&#128500;</span>
-    <img src=${thisImageUrl}>
-    <div>${thisImageDescription}</div>`;
+    <img src=${imageUrl}>
+    <div>${description}</div>`;
 
-    expandedImage.addEventListener('click', function(){
-        expandedImage.classList.remove('show');
+    expandedImage.addEventListener('click', () =>
+        expandedImage.classList.remove('show')
 
-    })
+    )
 }
