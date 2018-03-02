7436fce44759168f51f1936cc64b2c15bdb87356
Syknapse <syk.houdeib@gmail.com>
2018-03-02 21:05:55 +0100

    Add disable buttons
    Implement disable unused buttons to disable buttons when pages are not available or when last or first page is reached

--- script.js
+++ script.js
@@ -3,25 +3,43 @@ const output = document.getElementById('output-display');
 const nasaApi = 'https://images-api.nasa.gov/';
 let imageHref;
 let imageDescription;
-let next;
-let previous;
+let next = null;
+let previous = null;
 const searchResults = [];
+let searchLinks = [];
 
 input.addEventListener('keypress', function(event) {
     if(event.key === 'Enter'){
+        // reset prev/next
+        next = null;
+        previous = null;
+        // default value
+        if (input.value == '') {
+            input.value += 'earth';
+        }
         fetch(`${nasaApi}search?q=${input.value}&media_type=image`)
         .then(response => response.json())
         .then(data => {
             searchResults.splice(0, ...data.collection.items);
-            // stops an error if searchResults is empty
-            if (searchResults.length != 0) {
-                next = data.collection.links["0"].href;
+            searchLinks = data.collection.links;
+
+            // stops an error if no 'next' link available
+            if (data.collection.links && searchResults.length != 0) {
+                next = data.collection.links['0'].href;
             }
         })
         .then(displayResults)
         .catch(error => console.log(error))
     }
 });
+
+function displayResults() {
+    if (searchResults.length != 0) {
+        showImages();
+    } else {
+        output.innerHTML = `<div>Sorry, no matches. Try a different search</div>`;
+    }
+}
 
 function showImages() {
     let resultsDisplay = '';
@@ -35,31 +53,46 @@ function showImages() {
     `<button id="previous-btn">PREVIOUS</button>
     <button id="next-btn">NEXT</button>
     ${resultsDisplay}`;
+    disableUnusedButton();
     output.addEventListener('click', expand);
+}
+
+// if no pages available or has reached last/first page
+function disableUnusedButton() {
+    if (next == null || searchLinks.find(isNext) === undefined) {
+        document.getElementById('next-btn').disabled = true;
+    }
+    if (previous == null || searchLinks.find(isPrevious) === undefined) {
+        document.getElementById('previous-btn').disabled = true;
+    }
 }
 
 // prev/next page buttons
 output.addEventListener('click', function(event) {
-    if (event.target && event.target.matches('button#next-btn')) {
+    if (event.target && event.target.matches('button#next-btn') && next != null) {
         fetch(next)
         .then(response => response.json())
         .then(data => {
             searchResults.splice(0, ...data.collection.items)
-            next = data.collection.links.find(isNext).href;
-            previous = data.collection.links.find(isPrevious).href;
+            searchLinks = data.collection.links;
+
+            next = searchLinks.find(isNext).href;
+            previous = searchLinks.find(isPrevious).href;
         })
         .then(displayResults)
         .catch(error => console.log(error))
-    } else if (event.target && event.target.matches('button#previous-btn')) {
+    } else if (event.target && event.target.matches('button#previous-btn') && previous != null) {
         fetch(previous)
         .then(response => response.json())
         .then(data => {
-            searchResults.splice(0, ...data.collection.items)
+            searchResults.splice(0, ...data.collection.items);
+            searchLinks = data.collection.links;
+
             // Ensure page 1 (no previous href) is displayed
-            if (data.collection.links.find(isPrevious)) {
-                previous = data.collection.links.find(isPrevious).href;
+            if (searchLinks.find(isPrevious)) {
+                previous = searchLinks.find(isPrevious).href;
             }
-            next = data.collection.links.find(isNext).href;
+            next = searchLinks.find(isNext).href;
         })
         .then(displayResults)
         .catch(error => console.log(error))
@@ -71,14 +104,6 @@ function isNext(link) {
 }
 function isPrevious(link) {
     return link.prompt === 'Previous';
-}
-
-function displayResults() {
-    if (searchResults.length != 0) {
-        showImages();
-    } else {
-        output.innerHTML = `<div>Sorry , no matches. Try a different search</div>`;
-    }
 }
 
 function expand(event) {
